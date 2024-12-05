35d1c7de6c74987188771e6713d12f5238d651f4
RenierMS <146236147+RenierMS@users.noreply.github.com>
2024-12-05 10:49:47 -0500

    c1

---
+++ 1erPunto.py
@@ -0,0 +1,15 @@
+# Escribir un programa que imprima un mensaje en la consola
+print("Buen día Bienvenido")
+
+# Declarar variable de diferentes tipos
+Entero = int(input("Digita el numero entero: "))
+Decimal = float(input("Digita el numero decimal: "))
+Texto = input("Escriba el texto: ")
+
+# Realizar operaciones matematicas simples
+Suma = Entero + Decimal
+Resta = Entero - Decimal
+Multiplicacion = Entero * Decimal
+Division = Entero / Decimal if Decimal != 0 else "No se puede dividir por cero"
+
+# Concatenar cadena texto
