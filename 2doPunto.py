aa2036ba8af961d0669db46521813705d6346e7a
RenierMS <146236147+RenierMS@users.noreply.github.com>
2024-12-05 12:34:20 -0500

    c3

---
+++ 2doPunto.py
@@ -0,0 +1,9 @@
+# Pida al usuario un numero y determinar si es par o impar
+Numero = int(input("Ingresa numero: "))
+
+if Numero % 2 == 0:
+    print(f"Este numero {Numero} es par.")
+else:
+    print(f"Este numero {Numero} es impar.")
+
+# Implementar un bucle for para iterar sobre una lista de números e imprimir sus cuadrados.
