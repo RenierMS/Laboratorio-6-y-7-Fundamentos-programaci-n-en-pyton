97e174cbeaa5953d2f6f356bfda35e9a6c366e95
RenierMS <146236147+RenierMS@users.noreply.github.com>
2024-12-05 13:17:56 -0500

    c7

---
+++ 4toPunto.py
@@ -0,0 +1,12 @@
+# Script de Resolución de Problemas Simples
+# Desarrollar un programa que simule una calculadora básica, permitiendo al usuario realizar sumas, restas, multiplicaciones y divisiones.
+def Calculadora():
+    print ("Bienvenido a la calculadora")
+    print ("Selecciona la operacion")
+    print ("1. Suma")
+    print ("2. Resta")
+    print ("3. Multiplicacion")
+    print ("4. Division")
+    print ("5. Salir")
+
+# Crear un juego de adivinanza donde el programa genere un número aleatorio y el usuario deba adivinarlo, recibiendo pistas de "mayor" o "menor" en cada intento.
